be5bb2d93a418a417ceaa4dcb20258e1049a49c4
Aimee Liang <aimeeeliang@gmail.com>
2020-08-15 20:01:47 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,11 +9,11 @@ while row_index < src.length do
   element_index = 0
   while element_index < src[row_index].length do 
     if src[row_index][element_index].class == String
-      total << src[row_index][element_index]+""
+      total << src[row_index][element_index] +" "
     end
     element_index += 1
   end
   row_index += 1
 end
-total.to_s
+total
 end
