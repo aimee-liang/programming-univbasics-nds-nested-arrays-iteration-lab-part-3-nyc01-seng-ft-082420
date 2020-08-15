66e9e8ef75b10592d73c928305cf108726b18221
Aimee Liang <aimeeeliang@gmail.com>
2020-08-15 19:57:57 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -23,8 +23,8 @@ row_index = 0
 
 while row_index < src.count do 
   element_index = 0
-  while element_index < src[row_index].length do |element|
-    if element == ""
+  while element_index < src[row_index].length do 
+    if src[row_index][element_index] == ""
       total += src[row_index][element_index]
     end
     element_index += 1
